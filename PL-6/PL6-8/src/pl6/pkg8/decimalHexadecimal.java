c00ac75e5880370501b0f80d3821c47d76a70fe4
arceniouate <arcenio.uate@gmail.com>
2019-12-15 02:35:31 +0000

    exercicio 8-b

--- PL-6/PL6-8/src/pl6/pkg8/decimalHexadecimal.java
+++ PL-6/PL6-8/src/pl6/pkg8/decimalHexadecimal.java
@@ -5,8 +5,10 @@
  */
 package pl6.pkg8;
 
+import java.util.Scanner;
+
 /**
- *
+ *fazer ca conversão do metodo decimal para hexadecimal
  * @author arccenio
  */
 public class decimalHexadecimal {
@@ -15,20 +17,30 @@ public class decimalHexadecimal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int num = 1156;
-       
-       String n= Hexadeciaml( num);
-        System.out.println(n);
-    }
+        Scanner l = new Scanner(System.in);
+        int decimal;//numero decimal
 
-    public static String  Hexadeciaml(int decimal) {
+        do {
+            System.out.println("INSIRA O NUMERO DECIMAL");
+            decimal = l.nextInt();// leitura  apartir  do tecledo
+
+            String n = Hexadeciaml(decimal);
+            System.out.println(n);
+        } while (decimal != 0);
+    }
+/***
+ * Metodo para  converter o numero decimal para hexadecimal
+ * @param decimal numero decimal que sera convertido
+ * @return numeero decimal
+ */
+    public static String Hexadeciaml(int decimal) {
         int r = 0;
         String hex = "";
         char hexchars[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        while(decimal>0){
-          r = decimal % 16;
-          hex=hexchars[r]+hex;
-          decimal=decimal/16;
+        while (decimal > 0) {
+            r = decimal % 16;
+            hex = hexchars[r] + hex;
+            decimal = decimal / 16;
 
         }
         return hex;
