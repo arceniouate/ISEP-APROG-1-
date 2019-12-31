95dfe989b25d58e59a1d7a62907fdc54ceb26f40
arceniouate <arcenio.uate@gmail.com>
2019-12-31 14:58:24 +0000

    completo fatam comentarios

--- PL-7/PL7-4/src/pl7/pkg4/PL74.java
+++ PL-7/PL7-4/src/pl7/pkg4/PL74.java
@@ -22,16 +22,13 @@ public class PL74 {
         System.out.println("Enter the number of element of the array");
         nEle = l.nextInt();
         int[] v = new int[nEle];
-        int []vc=redyNumber(v);
-        for(int i=0;i<vc.length;i++){
-            System.out.println(vc[i]);
-            
+        int[] vc = redyNumber(v);
+        //System.out.println("numeros inseridos ");
+        for (int i = 0; i < vc.length; i++) {
+            System.out.printf(" %d ",vc[i]);
         }
         inversaoDaOrdemEle(v);
-               int[]p= inverterDireita(v,nEle);
-               for (int i = 0; i < p.length; i++) {
-                   System.out.println(p[i]);
-        }
+        inverterDireita(v, nEle);
 
     }
 
@@ -41,7 +38,7 @@ public class PL74 {
      *
      * @param v vetor para inseri os numeros inteiros
      */
-    public static int [] redyNumber(int[] v) {
+    public static int[] redyNumber(int[] v) {
         Scanner l = new Scanner(System.in);
         for (int i = 0; i < v.length; i++) {
             System.out.println("Enter the number ");
@@ -50,43 +47,53 @@ public class PL74 {
         return v;
     }
 
-  
-    
-    public static void  inversaoDaOrdemEle(int []v){
-        int left=0;
-        int rigth=v.length-1;
-        while(left<rigth){
-            int temp=v[left];
-            v[left]=v[rigth];
-            v[rigth]=temp;
+    public static void inversaoDaOrdemEle(int[] v) {
+        int left = 0;
+        int rigth = v.length - 1;
+        while (left < rigth) {
+            int temp = v[left];
+            v[left] = v[rigth];
+            v[rigth] = temp;
             //mover para direita ou para esquerda o valor apontado
             left++;
             rigth--;
             mostrar(v);
         }
-        
+
     }
-      public static void  mostrar(int []v){
-          
-        for (int i = 0; i <v.length; i++) {
-            System.out.printf("%d ",v[i]);
-            
+
+    public static void mostrar(int[] v) {
+        System.out.println("  ");
+        for (int i = 0; i < v.length; i++) {
+            System.out.printf(" %d ", v[i]);
+
         }
     }
-   public static int [] inverterDireita(int []v,int n){
-       int []r=new int [n];
-       for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-                   
 
-           r[1]= v[0];
-            r[2]=v[1];
-           r[0]= v[2];
-        
-       }
+    public static int[] inverterDireita(int[] v, int n) {
+        int[] r = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
 
-       }
-                      return r;
+                r[1] = v[0];
+                r[2] = v[1];
+                r[0] = v[2];
 
-   }
+            }
+
+        }
+        mostrarD(r);
+        return r;
+
+    }
+
+    public static void mostrarD(int[] p) {
+        System.out.println("  ");
+        for (int i = 0; i < p.length; i++) {
+
+            System.out.printf(" %d ", p[i]);
+
+        }
+
+    }
 }
